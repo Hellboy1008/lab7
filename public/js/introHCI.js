428155f9079160689a0b3fba243109a3af491e8d
Stephen Lee <srlee@ucsd.edu>
2020-02-14 15:06:07 -0800

    upload to heroku

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,12 +2,19 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+    initializePage();
 });
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+    $('.likeBtn').click(function(e) {
+        e.preventDefault();
+        ga("send", "event", "like", "click");
+    });
+    $('.likeCtr').click(function(e) {
+        e.preventDefault();
+        ga("send", "event", "like", "click");
+    });
 }
